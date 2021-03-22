4a4763145dc9f125fa9f76c7584717473f14cfe7
Alina Stewart <alinastewart@Alinas-iMac.local>
2021-03-22 12:01:05 -0700

    Add items into HashSet.

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+
+// Import the HashSet class
+import java.util.HashSet;
+
+public class Main {
+  public static void main(String[] args) {
+    HashSet<String> flowers = new HashSet<String>();
+    flowers.add("Rose");
+    flowers.add("Tulip");
+    flowers.add("Daisy");
+    flowers.add("Dahlia");
+    flowers.add("Daffodil");
+    System.out.println(flowers);
+  }
+}
